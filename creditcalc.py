743ff89088e9df717b245fd3eb6d52007cb2960a
seang <seang@moosecat.info>
2021-09-19 20:07:59 +0800

    added main structure

---
+++ creditcalc.py
@@ -0,0 +1,153 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+import sys
+import argparse
+from math import ceil, floor, log
+args = sys.argv
+
+parser = argparse.ArgumentParser(description='Process some integers.')
+
+parser.add_argument('-t', '--type', choices=['diff', 'annuity'], required=True,
+                    help='options are diff, annuity')
+parser.add_argument('-p', '--principal', type=int, required=True,
+                    help='the loan principal')
+parser.add_argument('-n', '--periods', type=int,
+                    help='denotes the number of months needed to repay the loan')
+parser.add_argument('-i', '--interest', type=float, required=True,
+                    help='annual interest rate')
+if parser.parse_known_args()[0].type.lower() == 'annuity':
+    parser.add_argument('-a', '--payment', type=float,
+                    help='the monthly annuity payment amount and only available with the annuity calculation')
+# TODO: requirement to check if at least 4 args
+
+
+def repayments(p, n):
+    settlement = ''
+    # calculate payment amounts (and settlement payment)
+    # payments = principal // number_of_months + additional month if required to settle
+    r = int(p / n)
+    is_float = bool(p % n)
+    if is_float:
+        r += 1
+    if r * n > p:
+        s = p - (r * (n - 1))
+        if s:
+            settlement = ' and the last payment = {}'.format(s)
+    r = 'Your monthly payment = {}{}.'.format(r, settlement)
+    return r
+
+
+def differentiated_payments(p, i, n):
+    d = []
+    for m in range(n):
+        d[m] = p/n + i * (p - ((p * (m - 1)) / n))
+    return d
+
+
+def annuity(p, i, n):
+    a = p * i * pow((1 + i), n) / (pow((1 + i), n) - 1)
+    a = ceil(a)
+    return a
+
+
+def no_repayments(p, i, a):
+    base = 1 + i
+    x = (a / (a - i * p))
+    n = log(x, base)
+    n = ceil(n)
+    return n
+
+
+def principal(a, i, n):
+    p = a / (i * pow((1 + i), n) / (pow((1 + i), n) - 1))
+    p = floor(p)
+    return p
+
+
+def get_interest():
+    a = float(input("Enter the loan interest:")) / 100
+    i = a / 12
+    return i
+
+
+def get_principal():
+    p = int(input("Enter the loan principal:"))
+    return p
+
+
+def get_monthly_payments():
+    m = int(input("Enter the monthly payment:"))
+    return m
+
+
+def get_no_periods():
+    n = int(input("Enter the number of periods:"))
+    return n
+
+
+def get_annuity():
+    a = float(input("Enter the annuity payment:"))
+    return a
+
+
+def get_years_months(n):
+    months = ''
+    years = ''
+    y = n // 12
+    m = n % 12
+    if y == 0:
+        years = ''
+    elif y == 1:
+        years = '1 year'
+    elif y > 1:
+        years = '{} years'.format(y)
+    if m == 0:
+        months = ''
+    elif m == 1:
+        months = '1 month'
+    elif m > 1:
+        months = '{} months'.format(m)
+    is_and = bool(months or years)
+    if is_and is True:
+        _and = 'and'
+    else:
+        _and = ''
+    message = 'It will take {} {} {} to repay this loan!'.format(years, _and, months)
+    return message
+
+
+calc_question = """What do you want to calculate?
+                type "n" for number of monthly payments,
+                type "a" for annuity monthly payment amount,
+                type "p" for loan principal:"""
+
+calculation = input(calc_question)
+
+# "n" for number of monthly payments
+if calculation == "n":
+    principal = get_principal()
+    annuity_payments = get_monthly_payments()
+    interest = get_interest()
+    no_repayments = no_repayments(principal, interest, annuity_payments)
+    print(get_years_months(no_repayments))
+# "a" for annuity monthly payment amount
+elif calculation == "a":
+    principal = get_principal()
+    periods = get_no_periods()
+    interest = get_interest()
+    annuity = annuity(principal, interest, periods)
+    print('Your monthly payment = {}!'.format(annuity))
+# "p" for loan principal:
+elif calculation == "p":
+    annuity = get_annuity()
+    periods = get_no_periods()
+    interest = get_interest()
+    principal = principal(annuity, interest, periods)
+    print('Your loan principal = {}!'.format(principal))
+
+
+
